4993cabcc52cf0d286c1b19cb8612c3936fe48de
Cory Redmond <ace@ac3-servers.eu>
2015-12-24 20:57:13 +0000

    I thought I fixed that.

--- src/main/java/co/ryred/secretsanta/commands/SecretSantaCommand.java
+++ src/main/java/co/ryred/secretsanta/commands/SecretSantaCommand.java
@@ -120,8 +120,8 @@ public class SecretSantaCommand implements CommandExecutor
 			}
 
 			User user;
-			if ( plugin.hasUser( offlinePlayer.getUniqueId() ) && plugin.getUser( uuid ) != null ) {
-				user = plugin.getUser( uuid );
+			if ( plugin.hasUser( offlinePlayer.getUniqueId() ) && plugin.getUser( offlinePlayer.getUniqueId() ) != null ) {
+				user = plugin.getUser( offlinePlayer.getUniqueId() );
 			}
 			else {
 				user = new User( plugin, offlinePlayer );
